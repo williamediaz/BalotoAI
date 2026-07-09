4121e07437544c657c00c2357e16c4283758d89a
williamediaz <williamediaz@yahoo.com>
2026-07-09 09:51:40 -0500

    Add Entity base class

---
+++ src/BalotoAI.Domain/Common/Entity.cs
@@ -0,0 +1,56 @@
+namespace BalotoAI.Domain.Common;
+
+/// <summary>
+/// Base class for all domain entities
+/// </summary>
+public abstract class Entity
+{
+    public int Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; protected set; }
+    public bool IsDeleted { get; protected set; }
+
+    protected Entity()
+    {
+    }
+
+    protected Entity(int id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity entity)
+            return false;
+
+        if (ReferenceEquals(this, obj))
+            return true;
+
+        if (GetType() != entity.GetType())
+            return false;
+
+        return Id == entity.Id && Id != 0;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Entity? left, Entity? right)
+    {
+        if (left is null && right is null)
+            return true;
+
+        if (left is null || right is null)
+            return false;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Entity? left, Entity? right)
+    {
+        return !(left == right);
+    }
+}
